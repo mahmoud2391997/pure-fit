5ebca498260368716c181e0af9f9bfe5b4bca15f
Mahmoud Elsayed <114721167+mahmoud2391997@users.noreply.github.com>
2025-11-12 21:44:10 +0200

    Update food.route.js

--- food.route.js
+++ food.route.js
@@ -1,8 +1,17 @@
 const express = require("express");
 const router = express.Router();
-const { getFoods,editFood,addFood } = require("./food.controller");
+const { getFoods, addFood, editFood, deleteFood } = require("./food.controller");
+
+// Get all foods
 router.get("/", getFoods);
+
+// Add new food
 router.post("/", addFood);
+
+// Edit existing food by ID
 router.put("/:id", editFood);
+
+// Delete food by ID
+router.delete("/:id", deleteFood);
 
 module.exports = router;
