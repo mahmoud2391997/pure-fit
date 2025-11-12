43d0f07da1c13ea8d4b6ff44a3a3f70587f925fd
Mahmoud Elsayed <114721167+mahmoud2391997@users.noreply.github.com>
2025-11-12 22:00:02 +0200

    Update drinks.controller.js

--- drinks.controller.js
+++ drinks.controller.js
@@ -1,47 +1,70 @@
 const Drinks = require("./drinks.model");
 const Mongoose = require("mongoose");
 
+// Get all drinks
 const getDrinks = async (req, res) => {
   try {
     const drinks = await Drinks.find({});
     res.json(drinks);
   } catch (error) {
-    res.status(500).json("internal server error");
-    return null;
+    console.error(error);
+    res.status(500).json("Internal server error");
   }
 };
-const addDrink = async (req, res) => {
- const food = req.body
- console.log(food)
-  try {
-    const response = await Drinks.create({...food});
-     console.log(response)
 
+// Add a new drink
+const addDrink = async (req, res) => {
+  const drink = req.body;
+  try {
+    const response = await Drinks.create(drink);
     res.json(response);
   } catch (error) {
-    res.status(500).json("internal server error");
-    return null;
+    console.error(error);
+    res.status(500).json("Internal server error");
   }
 };
-const editDrink= async (req, res) => {
-    const Id = req.params.id;
 
- const drink = req.body
+// Edit a drink by ID
+const editDrink = async (req, res) => {
+  const id = req.params.id;
+  const drink = req.body;
+
   try {
     const response = await Drinks.updateOne(
-      { _id: new Mongoose.Types.ObjectId(Id) },
+      { _id: new Mongoose.Types.ObjectId(id) },
       { $set: drink },
       { upsert: false }
     );
-    console.log(res)
     res.json(response);
-        return null;
-
   } catch (error) {
-    res.status(500).json("internal server error");
-    return null;
+    console.error(error);
+    res.status(500).json("Internal server error");
+  }
+};
+
+// Delete a drink by ID
+const deleteDrink = async (req, res) => {
+  const id = req.params.id;
+
+  try {
+    const response = await Drinks.deleteOne({
+      _id: new Mongoose.Types.ObjectId(id),
+    });
+
+    if (response.deletedCount === 0) {
+      return res.status(404).json({ message: "Drink not found" });
+    }
+
+    res.json({ message: "Drink deleted successfully" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json("Internal server error");
   }
 };
 
 module.exports = {
-getDrinks,editDrink,addDrink};
+  getDrinks,
+  addDrink,
+  editDrink,
+  deleteDrink,
+};
