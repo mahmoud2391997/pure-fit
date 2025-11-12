2b7b28ea87caf19d8bfaf22051aa32fa2b623a0b
Mahmoud Elsayed <114721167+mahmoud2391997@users.noreply.github.com>
2025-11-12 21:54:17 +0200

    Update category.route.js

--- category.route.js
+++ category.route.js
@@ -1,10 +1,22 @@
 const express = require("express");
 const router = express.Router();
-const { editCategory,addCategory ,getCategories} = require("./category.controller");
+const {
+  getCategories,
+  addCategory,
+  editCategory,
+  deleteCategory,
+} = require("./category.controller");
+
+// Get all categories
 router.get("/", getCategories);
+
+// Add a new category
 router.post("/", addCategory);
 
+// Edit an existing category by ID
 router.put("/:id", editCategory);
 
+// Delete a category by ID
+router.delete("/:id", deleteCategory);
 
 module.exports = router;
